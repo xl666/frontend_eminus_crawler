9a8294207686a3a45aae98e181cb7af273ee45a7
xl666 <xavier120@hotmail.com>
2020-04-23 18:54:20 -0500

    -Se agrega una app para conexiones a BD -Se agrega soporte de cifrado a script de inicio de servidor y de migración

--- src/front_end/front_end/settings.py
+++ src/front_end/front_end/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bd',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'front_end.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
+
 
 
 # Password validation
--- src/front_end/migrar.sh
+++ src/front_end/migrar.sh
@@ -4,4 +4,5 @@ for var in $(ccrypt -d -c settings.env.cpt); do
     export "$var"
 done
 
-python manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
