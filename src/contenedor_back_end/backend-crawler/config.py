e6c8dae8da8ddca7df9ed37559b9112bb22043e9
xl666 <xavier120@hotmail.com>
2020-06-06 13:34:09 -0500

    -Se reestructura lo referente a docker-compose para que considere todos los subproyectos -Se agrega Dockerfile del subproyecto de back-end

--- src/contenedor_back_end/backend-crawler/config.py
+++ src/contenedor_back_end/backend-crawler/config.py
@@ -10,8 +10,9 @@ def configure():
     options = webdriver.ChromeOptions()
     #options.binary_location = '/usr/bin/chromium'
     options.add_argument('headless')
+    options.add_argument('no-sandbox')
     options.add_argument('window-size=1800x1024')
-    options.add_argument("--log-level=3");
-    options.add_argument("--silent");
+    options.add_argument("--log-level=3")
+    options.add_argument("--silent")
     driver = webdriver.Chrome(options=options)
     return driver
---
+++ src/contenedor_back_end/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=/code:$PATH
+
+sleep 15
+
+# verificar existencia de usuario cliente en BD
+
+
+# Lanzar servidor
+
+echo "Hola mundo"
